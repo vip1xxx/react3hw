d86524ca464dc34f9b4cf2f61c29d11102abe227
x <fedorenkomcbook@gmail.com>
2023-10-27 16:44:22 +0200

    pushingg

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import {CarsContainer} from "./components/CarsContainer/CarsContainer";
+
+const App = () => {
+  return (
+      <div>
+        <CarsContainer/>
+      </div>
+  );
+};
+
+export {App};
